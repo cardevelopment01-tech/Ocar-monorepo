0ca382fa67803dd2e45603bf6dd788017a774a34
Sujal Kumar Ghosh <sujalkrghosh@gmail.com>
2026-08-15 13:53:00 +0530

    style(infra): match variable/tag ordering conventions in bootstrap module

--- infra/terraform/bootstrap/variables.tf
+++ infra/terraform/bootstrap/variables.tf
@@ -4,14 +4,14 @@
 # an account-wide singleton, not something that differs between prod and
 # staging. That's the whole point of this module's existence.
 
-variable "project_name" {
-  type        = string
-  description = "Short project name, used as a prefix/tag on resources"
-  default     = "ocar"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS region everything gets created in"
   default     = "ap-south-1"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Short project name, used as a prefix/tag on resources"
+  default     = "ocar"
 }
